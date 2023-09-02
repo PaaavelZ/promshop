e110506e299474079155e50872a983394f98f872
root <root@hostname.kz>
2023-09-03 04:08:59 +0600

    Fix gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -1,9 +1,4 @@
 import multiprocessing
 
-<<<<<<< HEAD
 bind = "0.0.0.0:8000"
 workers = 2 * multiprocessing.cpu_count() + 1
-=======
-bind = "0.0.0.0:8080"
-workers = 2 * multiproicessing.cpu_count() + 1
->>>>>>> 0147d4ea4cca9203652d4ab32ca0631afce1185d
