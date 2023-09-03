9455422bdbf1f32864518e365bf451b204b7d944
Your Name <you@example.com>
2023-09-03 06:23:01 +0600

    Not final

--- aaps/settings.py
+++ aaps/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["contour-pro.kz",]
 
 
 # Application definition
