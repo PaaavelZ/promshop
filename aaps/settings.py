b119bf2b2721847860f2f63134e19e1530505157
Evgeny Namsaraev <enamsaraev13@icloud.com>
2023-09-04 16:44:23 +0300

    fix:admin

--- aaps/settings.py
+++ aaps/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = env.list("https://contour-pro.kz/")
+CSRF_TRUSTED_ORIGINS = env.list("https://contour-pro.kz/")
 
 
 # Application definition
