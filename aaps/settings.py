d40b44a9e0a6f2ffd56eeb37b19cfc1cca9d7686
Evgeny Namsaraev <enamsaraev13@icloud.com>
2023-09-29 18:42:39 +0300

    mic mail

--- aaps/settings.py
+++ aaps/settings.py
@@ -138,7 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-RECIPIENTS_EMAIL = ['enamsaraev@bk.ru', 'andr.gosh@yandex.ru', 'contourprokz@gmail.com',]
+RECIPIENTS_EMAIL = ['enamsaraev@bk.ru', 'andr.gosh@yandex.ru', 'contourprokz@gmail.com', 'bukkd@yandex.ru',]
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
